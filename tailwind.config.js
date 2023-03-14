baadac160ed2b42008cb59ddbcbd5bf49f8d4274
AndradeJaum <andradejaum@gmail.com>
2023-03-14 14:43:42 -0300

    Initial commit, configured and ready to code

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      purple: "#8284FA",
+      purple_dark: "#5E60CE",
+      blue: "#4EA8DE",
+      blue_dark: "#1E6F9F",
+      gray700: "#0D0D0D",
+      gray600: "#1A1A1A",
+      gray500: "#262626",
+      gray400: "#333333",
+      gray300: "#808080",
+      gray200: "#D9D9D9",
+      gray100: "#F2F2F2",
+      danger: "#E25858",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
